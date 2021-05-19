874e4389123276fa0b1beb092b8738d291074d1a
A.vagin <100lototest@gmail.com>
2021-05-19 18:11:31 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+public class MainClass {
+
+    public static int localNumber = 14;
+
+    public int getLocalNumber()
+    {
+        return localNumber;
+    }
+}
---
+++ src/mainClassTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class mainClassTest extends MainClass
+{
+
+    @Before
+    public void textStartSession()
+    {
+        System.out.println("Session start");
+    }
+
+    @After
+    public void textFinishtSession()
+    {
+        System.out.println("Session finished");
+    }
+
+    @Test
+    public void testGetLocalNumber() {
+        int expected = 14;
+        int actual = this.getLocalNumber();
+        System.out.println("Start first test");
+        Assert.assertTrue("The number should be == 14, but your is " + actual,expected == actual);
+
+    }
+
+
+}
